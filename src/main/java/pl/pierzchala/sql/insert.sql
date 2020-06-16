f3102f49771a60d6e5b21dde666765927625cdd8
Aneta <adam.pierzchala.muzyk@gmail.com>
2020-06-17 00:08:20 +0200

    [developWin] 20_06_16_23_01

--- src/main/java/pl/pierzchala/sql/insert.sql
+++ src/main/java/pl/pierzchala/sql/insert.sql
@@ -108,17 +108,3 @@ INSERT INTO users (login,password,first_name,last_name,city, street,home_number,
 ("kot","$2a$10$b4btD12d.UtjuTMTUqQKV.6o6FNACZEsksGBvpyI6RswwOT/UxTtG","Paulina","Podsiadło","Gdańsk","Mysia","335/8","paulina.paulina@email.pl",null,false ,false ),
 ("pies","$2a$10$b4btD12d.UtjuTMTUqQKV.6o6FNACZEsksGBvpyI6RswwOT/UxTtG","Aneta","Wierzbicka","Szczecin","Krypska","30/2","aneta@email.pl",null,false,false );
 
-/*
-INSERT INTO `plan` (`id`, `name`, `description`, `created`, `admin_id`) VALUES
-(1, 'Plan Jarski', NULL, '2018-10-17 14:27:05', 1);
-
-INSERT INTO `recipe` (`id`, `name`, `ingredients`, `description`, `created`, `updated`, `admin_id`, `preparation_time`, `preparation`) VALUES
-(1, 'Przepis 1', 'sałata', 'Opis przepisu 1', '2018-10-17 00:00:00', '2018-10-17 14:24:44', 1, 30,'Sałaty dokładnie myję i jeszcze dokładniej suszę - nie powinno być na liściach żadnych kropelek wody.'),
-(2, 'Przepis 2', 'sałata', 'Opis przepisu 2', '2018-10-16 00:00:00', '2018-10-17 14:24:44', 1, 30,'Sałaty dokładnie myję i jeszcze dokładniej suszę - nie powinno być na liściach żadnych kropelek wody.'),
-(3, 'Przepis 3', 'sałata', 'Opis przepisu 3', '2018-10-24 00:00:00', '2018-10-17 14:24:44', 1, 30,'Sałaty dokładnie myję i jeszcze dokładniej suszę - nie powinno być na liściach żadnych kropelek wody.');
-*/
--- create - tworzy tabele na podstawie adnotacji encji, nie nadpisuje zmian
---           none - nie wykonuje żadnych operacji
---           drop-and-create - usunie a następnie utworzy, przy pracy uzywamy tego
---           drop - usunie elementy bazy danych zgodne z adnotacjami
---           update - !!! nie zawsze poprawnie zmieni
